b26ae974c5e52fe24d4ef2aeaa9cab1b7f4568a6
end5 <16655171+end5@users.noreply.github.com>
2017-09-13 21:00:00 -0700

    New Plot class. Dont know what todo with yet.

---
+++ src/classes/Plot.ts
@@ -0,0 +1,12 @@
+export default class Plot {
+    explored = false;
+    foundForest = false;
+    foundDesert = false;
+    foundMountain = false;
+    foundLake = false;
+    whitney = 0;
+    monk = 0;
+    sand = 0;
+    giacomo = 0;
+
+}
