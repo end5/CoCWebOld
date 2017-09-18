bec0fd01b35c946a6b83a5ded5671538ad9ca697
end5 <16655171+end5@users.noreply.github.com>
2017-09-17 22:17:32 -0700

    Updated Libraries to reflect the changes to perks.

--- src/classes/Game/Libraries.ts
+++ src/classes/Game/Libraries.ts
@@ -1,18 +1,20 @@
 ﻿import Library from "../Utilities/Library";
 import ItemTypeLib from "../Items/ItemTypeLib";
-import ConsumableLib from "../Items/Consumable/ConsumableLib";
+import ConsumableLib from "../Items/Consumables/ConsumableLib";
 import UseableLib from "../Items/UseableLib";
 import WeaponLib from "../Items/Weapons/WeaponLib";
-import ArmorLib from "../Items/Armor/ArmorLib";
-import PerkDescLib from "../Perks/PerkDescLib";
+import ArmorLib from "../Items/Armors/ArmorLib";
+import StatusAffectDescLib from "../Effects/StatusAffectDescLib";
+import PerkDescLib from "../Effects/PerkDescLib";
 
 export default class Libraries {
-    public items: ItemTypeLib;
-    public weapons: WeaponLib;
-    public armor: ArmorLib;
-    public consumables: ConsumableLib;
-    public useables: UseableLib;
-    public perkDesc: PerkDescLib;
+    public readonly items: ItemTypeLib;
+    public readonly weapons: WeaponLib;
+    public readonly armor: ArmorLib;
+    public readonly consumables: ConsumableLib;
+    public readonly useables: UseableLib;
+    public readonly statusAffectDesc: StatusAffectDescLib;
+    public readonly perkDesc: PerkDescLib;
 
     public constructor() {
         this.items = new ItemTypeLib();
@@ -20,6 +22,7 @@ export default class Libraries {
         this.armor = new ArmorLib();
         this.consumables = new ConsumableLib();
         this.useables = new UseableLib();
+        this.statusAffectDesc = new StatusAffectDescLib();
         this.perkDesc = new PerkDescLib();
     }
 }
