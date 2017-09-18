1b9ce6f0237baac864af9770e31bafea1e96c472
end5 <16655171+end5@users.noreply.github.com>
2017-09-17 22:26:46 -0700

    Modified EffectDescriptions to be readonly.

--- src/classes/Effects/EffectDescription.ts
+++ src/classes/Effects/EffectDescription.ts
@@ -2,30 +2,14 @@ import KeyObject from "../Utilities/KeyObject";
 import Effect from "./Effect";
 
 export default class EffectDescription extends KeyObject {
-    private _name: string;
-    private _desc: string;
-    private _longDesc: string;
+    public readonly name: string;
+    public readonly desc: string;
+    public readonly longDesc: string;
     constructor(objectKey: string, name: string, desc: string, longDesc: string = null) {
         super(objectKey);
-        this._name = name;
-        this._desc = desc || this._name;
-        this._longDesc = longDesc || this._desc;
-    }
-
-    public get name(): string {
-        return this._name;
-    }
-
-    public desc(params: Effect<EffectDescription> = null): string {
-        return this._desc;
-    }
-
-    public get longDesc(): string {
-        return this._longDesc;
-    }
-
-    public toString(): string {
-        return "\"" + this._name + "\"";
+        this.name = name;
+        this.desc = desc || this.name;
+        this.longDesc = longDesc || this.desc;
     }
 }
 
