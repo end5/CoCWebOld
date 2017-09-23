433f45595fbe58a44446babc60cb029d37353fd5
end5 <16655171+end5@users.noreply.github.com>
2017-09-22 22:05:48 -0700

    Divided up old inventory code and implemented new ways to handle inventory

---
+++ src/classes/display/InventoryDisplay.ts
@@ -0,0 +1,191 @@
+import Player from "../Player";
+import Inventory from "../Inventory/Inventory";
+import Item from "../Items/Item";
+import MainScreen, { ClickEventListener } from "./MainScreen";
+import Game from "../Game/Game";
+import ItemStack from "../Items/ItemStack";
+import CombatMenu from "./CombatMenu";
+import PlayerMenu from "./PlayerMenu";
+import PlayerInventoryMenu from "./PlayerInventoryMenu";
+
+/* better inventory system
+    other inv = null
+    helditem = null
+    page = 0
+
+    buttons
+    0	1	2	3	4
+    5	6	7	8	9
+
+    players inv
+        9 = back
+        8 = swap - unavailable in combat
+        7 = unequip weapon
+
+    swap menu - chests and racks open this menu
+        click = pick up itemstack
+        9 = back
+        if (other inv)
+            8 = 
+        7 = abandon
+
+    if (inv.length > 7)
+        6 = next page
+    if (page == 2)
+        6 = prev page
+*/
+type ItemCallback = (player: Player, inventory: Inventory<Item>, itemSlot: number) => void;
+
+export default class InventoryDisplay {
+    private static prevMenu: ClickEventListener;
+    private static reverseAction: Function;
+    private static heldItem: ItemStack<Item>;
+    public static isHoldingItem(): boolean {
+        return InventoryDisplay.heldItem != null;
+    }
+
+    /**
+        if not holding item
+            useItem(player, inv, itemslot)
+                if (inv.get(itemSlot).canUse)
+                    item = inv.remove(itemslot, 1)
+                    item.use
+                    item.useText
+            display player inventory
+        else
+            replaceItem(player, inv, itemslot)
+                inv.get(itemslot) = helditem
+            putback
+                reverseAction
+                reverseAction = null
+                display other inventory
+            use now
+                helditem.use
+                helditem.useText
+                reverseAction = null
+                display other inventory
+            abandon
+                helditem = null
+                reverseAction = null
+                display other inventory
+    */
+    public static displayPlayersInventory(player: Player) {
+        if (!InventoryDisplay.isHoldingItem()) {
+            InventoryDisplay.displayInventoryButtons(player, player.inventory.items, InventoryDisplay.useItem, PlayerInventoryMenu.display);
+        }
+        else {
+            MainScreen.text("There is no room for " + InventoryDisplay.heldItem.item.longName + " in your inventory.  You may replace the contents of a pouch with " + InventoryDisplay.heldItem.item.longName + " or abandon it.");
+            InventoryDisplay.displayInventoryButtons(player, player.inventory.items, InventoryDisplay.putHeldItemIntoInventory, function () {
+                MainScreen.doNext(InventoryDisplay.prevMenu);
+            });
+            MainScreen.addButton(7, "Put Back", function () {
+                InventoryDisplay.reverseAction();
+                InventoryDisplay.reverseAction = null;
+                MainScreen.doNext(InventoryDisplay.prevMenu);
+            });
+            MainScreen.addButton(8, "Use Now", function () {
+                if (InventoryDisplay.heldItem.item.canUse(player)) {
+                    InventoryDisplay.heldItem.item.use(player);
+                    InventoryDisplay.heldItem.item.useText(player);
+                    InventoryDisplay.heldItem = null;
+                    InventoryDisplay.reverseAction = null;
+                    MainScreen.doNext(InventoryDisplay.prevMenu);
+                }
+            });
+            MainScreen.addButton(9, "Abandon", function () {
+                InventoryDisplay.heldItem = null;
+                InventoryDisplay.reverseAction = null;
+                MainScreen.doNext(InventoryDisplay.prevMenu);
+            });
+        }
+
+    }
+
+    /*
+        if room in players inventory
+            putItemInPlayerEmptySlot(player, inv, itemslot)
+                emptyslot = player.getemptyslot()
+                emptyslot = helditem
+            display other inv
+        else
+            holdItem(player, inv, itemslot)
+                helditem = inv.remove(itemslot, 1)
+                reverseAction = function()
+                    itemstack = inv.get(itemslot)
+                    if (itemstack.quantity == 0)
+                        itemstack = helditem
+                    else
+                        itemstack.quantity += helditem.quantity
+                        helditem = null
+            display players inv
+    */
+    public static displayOtherInventory(player: Player, otherInventory: Inventory<Item>) {
+        if (player.inventory.items.hasEmptySlot()) {
+            InventoryDisplay.displayInventoryButtons(player, otherInventory, InventoryDisplay.putHeldItemInPlayerEmptySlot, function () {
+                MainScreen.doNext(InventoryDisplay.prevMenu);
+            });
+        }
+        else {
+            InventoryDisplay.displayInventoryButtons(player, otherInventory, InventoryDisplay.holdItem, function () {
+                MainScreen.doNext(PlayerInventoryMenu.display);
+            });
+        }
+    }
+
+    public static unequipFunction(player: Player): ClickEventListener {
+        return function () {
+            player.inventory.weapon.removeText();
+            InventoryDisplay.heldItem = new ItemStack(player.inventory.weapon.unequip(player), 1);
+            PlayerInventoryMenu.display(player);
+        };
+    }
+
+    private static useItem(player: Player, inventory: Inventory<Item>, itemSlot: number) {
+        let itemStack = inventory.get(itemSlot);
+        if (itemStack && itemStack.item.canUse(player)) {
+            itemStack = itemStack.split(1);
+            itemStack.item.use(player);
+            itemStack.item.useText(player);
+        }
+    }
+
+    private static putHeldItemIntoInventory(player: Player, inventory: Inventory<Item>, itemSlot: number) {
+
+        if (inventory.get(itemSlot).item == InventoryDisplay.heldItem.item)
+            MainScreen.text("You discard " + InventoryDisplay.heldItem.item.longName + " from the stack to make room for the new one.");
+        else if (inventory.get(itemSlot).quantity == 1)
+            MainScreen.text("You throw away " + inventory.get(itemSlot).item.longName + " and replace it with " + InventoryDisplay.heldItem.item.longName + ".");
+        else
+            MainScreen.text("You throw away " + inventory.get(itemSlot).item.longName + "(x" + inventory.get(itemSlot).quantity + ") and replace it with " + InventoryDisplay.heldItem.item.longName + ".");
+        inventory.set(itemSlot, InventoryDisplay.heldItem);
+        InventoryDisplay.heldItem = null;
+    }
+
+    private static putHeldItemInPlayerEmptySlot(player: Player, inventory: Inventory<Item>, itemSlot: number) {
+        InventoryDisplay.putHeldItemIntoInventory(player, player.inventory.items, player.inventory.items.getEmptySlotIndex());
+    }
+
+    private static holdItem(player: Player, inventory: Inventory<Item>, itemSlot: number) {
+        InventoryDisplay.heldItem = inventory.get(itemSlot).split(1);
+        InventoryDisplay.reverseAction = function () {
+            let itemStack = inventory.get(itemSlot);
+            if (itemStack.quantity == 0)
+                inventory.set(itemSlot, InventoryDisplay.heldItem);
+            else
+                itemStack.quantity += InventoryDisplay.heldItem.quantity;
+            InventoryDisplay.heldItem = null;
+        }
+    }
+
+    private static displayInventoryButtons(player: Player, displayedInv: Inventory<Item>, itemCallback: ItemCallback, clickFunction: ClickEventListener) {
+        MainScreen.hideButtons();
+        for (let index = 0; index < displayedInv.length; index++) {
+            if (displayedInv.get(index).quantity > 0) {
+                MainScreen.addButton(index, (displayedInv.get(index).item.shortName + " x" + displayedInv.get(index).quantity), function () {
+                    itemCallback(player, displayedInv, index);
+                    clickFunction(player);
+                });
+            }
+        }
+    }
+}
---
+++ src/classes/display/PlayerInventoryMenu.ts
@@ -0,0 +1,74 @@
+import MainScreen from "./MainScreen";
+import Game from "../Game/Game";
+import Flags, { FlagEnum } from "../Game/Flags";
+import Player from "../Player";
+import Inventory from "../Inventory/Inventory";
+import Item from "../Items/Item";
+import PlayerMenu from "./PlayerMenu";
+import CombatMenu from "./CombatMenu";
+import InventoryDisplay from "./InventoryDisplay";
+
+export default class PlayerInventoryMenu {
+    public static display(player: Player) {
+        let inventory: Inventory<Item> = player.inventory.items;
+        let foundItem: Boolean = false;
+
+        spriteSelect(-1);
+
+        MainScreen.hideTopButtons();
+        MainScreen.clearText();
+        MainScreen.text("<b><u>Equipment:</u></b>\n");
+        MainScreen.text("<b>Weapon</b>: " + player.inventory.weapon.displayname + " (Attack - " + player.inventory.weapon.attack + ")\n");
+        MainScreen.text("<b>Armor : </b>" + player.inventory.armor.displayName + " (Defense - " + player.inventory.armor.defense + ")\n");
+        if (player.keyItems.length > 0)
+            MainScreen.text("<b><u>\nKey Items:</u></b>\n");
+        for (let index = 0; index < player.keyItems.length; index++)
+            MainScreen.text(player.keyItems[index].objectKey + "\n");
+
+        MainScreen.hideTopButtons();
+        InventoryDisplay.displayPlayersInventory(player);
+
+        if (player.inventory.weapon != Game.libraries.weapons.get("Fists")) {
+            MainScreen.addButton(5, "Unequip", InventoryDisplay.unequipFunction(player));
+        }
+
+        if (!Game.inCombat && inDungeon == false && inRoomedDungeon == false) {
+            if (Game.sceneManager.nieveHoliday() && Flags.get(FlagEnum.NIEVE_STAGE) > 0 && Flags.get(FlagEnum.NIEVE_STAGE) < 5) {
+                if (Flags.get(FlagEnum.NIEVE_STAGE) == 1)
+                    MainScreen.text("\nThere's some odd snow here that you could do something with...\n");
+                else MainScreen.text("\nYou have a snow" + Game.sceneManager.nieveMF("man", "woman") + " here that seems like it could use a little something...\n");
+                MainScreen.addButton(6, "Snow", Game.sceneManager.nieveBuilding);
+                foundItem = true;
+            }
+            if (Flags.get(FlagEnum.FUCK_FLOWER_KILLED) == 0 && Flags.get(FlagEnum.FUCK_FLOWER_LEVEL) >= 1) {
+                if (Flags.get(FlagEnum.FUCK_FLOWER_LEVEL) == 4) MainScreen.text("\nHolli is in her tree at the edges of your camp.  You could go visit her if you want.\n");
+                MainScreen.addButton(7, (Flags.get(FlagEnum.FUCK_FLOWER_LEVEL) >= 3 ? "Tree" : "Plant"), Game.sceneManager.holliScene.treeMenu);
+                foundItem = true;
+            }
+            if (player.hasKeyItem("Dragon Egg")) {
+               Game.sceneManager.emberScene.emberCampDesc();
+               MainScreen.addButton(8, "Egg", Game.sceneManager.emberScene.emberEggInteraction);
+                foundItem = true;
+            }
+        }
+        if (!foundItem) {
+            MainScreen.text("\nYou have no usable items.");
+            MainScreen.doNext(PlayerMenu.display);
+            return;
+        }
+        if (Game.inCombat && player.statusAffects.has("Sealed") && player.statusAffects.get("Sealed").value1 == 3) {
+            MainScreen.text("\nYou reach for your items, but you just can't get your pouches open.  <b>Your ability to use items was sealed, and now you've wasted a chance to attack!</b>\n\n");
+            Game.enemyAI();
+            return;
+        }
+
+        MainScreen.text("\nWhich item will you use?");
+
+        if (!InventoryDisplay.isHoldingItem()) {
+            if (Game.inCombat)
+                MainScreen.addBackButton("Back", CombatMenu.display);
+            else
+                MainScreen.addBackButton("Back", PlayerMenu.display);
+        }
+    }
+}
