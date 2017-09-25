55d0cb326deab851bb1e7f20b97df3ff2faabea2
end5 <16655171+end5@users.noreply.github.com>
2017-09-24 22:59:26 -0700

    EmberEgg became DragonEgg

--- src/classes/Items/Consumables/DragonEgg.ts
+++ src/classes/Items/Consumables/DragonEgg.ts
@@ -2,10 +2,12 @@ import Consumable from "./Consumable";
 import Player from "../../Player";
 import MainScreen from "../../display/MainScreen";
 import Utils from "../../Utilities/Utils";
+import StatusAffect from "../../Effects/StatusAffect";
+import StatModifiers from "../../Modifiers/StatModifiers";
 
-export default class EmberEgg extends Consumable {
+export default class DragonEgg extends Consumable {
     public constructor() {
-        super("Smart T", "Scholars T.", "a cup of scholar's tea", 0, "This powerful brew supposedly has mind-strengthening effects.");
+        super("DrgnEgg", "DrgnEgg", "an unfertilized dragon egg", 0, "A large, solid egg, easily the size of your clenched fist.  Its shell color is reddish-white, with blue splotches.");
     }
 
     public use(player: Player) {
@@ -15,12 +17,11 @@ export default class EmberEgg extends Consumable {
         MainScreen.text("You crack the shell easily and swallow the large yolk and the copious amounts of albumen - the yolk is blue, while the rest is crimson-tinted.  It tastes like... well, it tastes mostly of spiced mint, you think.");
         if (player.perks.has("Dragonfire")) {
             if (player.statusAffects.has("DragonBreathCooldown")) player.statusAffects.remove("DragonBreathCooldown");
-            else {
-                if (player.findStatusAffect(StatusAffects.DragonBreathBoost) < 0) player.statusAffects.add(new StatusAffect("DragonBreathBoost", 0, 0, 0, 0)));
-            }
+            else if (!player.statusAffects.has("DragonBreathBoost"))
+                player.statusAffects.add(new StatusAffect("DragonBreathBoost", 0, 0, 0, 0));
             //(if PC has breath weapon)
             MainScreen.text("\n\nA sudden surge of energy fills your being and you feel like you could blast anything to atoms with a single breath, like the mighty dragons of legends.");
         }
-        fatigue(-20);
+        StatModifiers.fatigue(player, -20);
     }
 }
