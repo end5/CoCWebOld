fdc13dad7e1b272a97f5380d8ef4b3c462c54745
end5 <16655171+end5@users.noreply.github.com>
2017-09-25 23:48:47 -0700

    Removed unused ItemDescLib. Added DefaultValue to Item. Updated Libraries.

--- src/classes/Game/Libraries.ts
+++ src/classes/Game/Libraries.ts
@@ -1,27 +1,24 @@
 ﻿import Library from "../Utilities/Library";
-import ItemTypeLib from "../Items/ItemTypeLib";
 import ConsumableLib from "../Items/Consumables/ConsumableLib";
-import ItemLib from "../Items/UseableLib";
 import WeaponLib from "../Items/Weapons/WeaponLib";
 import ArmorLib from "../Items/Armors/ArmorLib";
 import StatusAffectDescLib from "../Effects/StatusAffectDescLib";
 import PerkDescLib from "../Effects/PerkDescLib";
+import MaterialLib from "../Items/MaterialLib";
 
 export default class Libraries {
-    public readonly items: ItemTypeLib;
     public readonly weapons: WeaponLib;
     public readonly armor: ArmorLib;
     public readonly consumables: ConsumableLib;
-    public readonly useables: ItemLib;
+    public readonly materials: MaterialLib;
     public readonly statusAffectDesc: StatusAffectDescLib;
     public readonly perkDesc: PerkDescLib;
 
     public constructor() {
-        this.items = new ItemTypeLib();
         this.weapons = new WeaponLib();
         this.armor = new ArmorLib();
         this.consumables = new ConsumableLib();
-        this.useables = new ItemLib();
+        this.materials = new MaterialLib();
         this.statusAffectDesc = new StatusAffectDescLib();
         this.perkDesc = new PerkDescLib();
     }
--- src/classes/Items/Item.ts
+++ src/classes/Items/Item.ts
@@ -2,7 +2,8 @@ import ItemDesc from "./ItemDesc";
 import Player from "../Player";
 
 export default abstract class Item extends ItemDesc {
-    constructor(key: string, shortName: string = null, longName: string = null, value: number = 0, description: string = null) {
+    public static readonly DefaultValue: number = 6;
+    constructor(key: string, shortName: string = null, longName: string = null, value: number = Item.DefaultValue, description: string = null) {
         super(key, shortName, longName, value, description);
     }
 
