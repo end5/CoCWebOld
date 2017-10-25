727ae49b3c5ffedbecc88d06dc3874a315b97cdf
end5 <16655171+end5@users.noreply.github.com>
2017-10-24 21:12:23 -0700

    Fixed logic bug

--- src/classes/Combat/CombatManager.ts
+++ src/classes/Combat/CombatManager.ts
@@ -24,23 +24,41 @@ class CombatParty {
     public readonly allMembers: Character[];
     public ableMembers: Character[];
     public defeatEvents: DefeatEvent[];
-    private defeatedMember: boolean;
 
     public constructor(party: Character[]) {
         this.allMembers = party;
         this.ableMembers = party.slice();
     }
 
+    public readyPartyMember(): Character {
+        return this.ableMembers[0];
+    }
+
     public lostFight(combatEndType: CombatEndType, victor: Character) {
-        this.defeatedMember = true;
         this.defeatEvents.push(new DefeatEvent(victor, this.ableMembers[0], combatEndType));
     }
 
-    public updateMembers() {
-        if (this.defeatedMember)
-            this.ableMembers.shift();
-        else
+    public readyNextPartyMember() {
+        if (this.ableMembers.length > 0)
             this.ableMembers.push(this.ableMembers.shift());
+    }
+
+    public resolveAttacker(attacker: Character) {
+        for (let index: number = 0; index < this.ableMembers.length; index++) {
+            const defender = this.ableMembers[index];
+            if (defender.stats.HP < 1) {
+                attacker.combat.claimsVictory(CombatEndType.HP, defender);
+                this.defeatEvents.push(new DefeatEvent(attacker, defender, CombatEndType.HP));
+            }
+            else if (defender.stats.lust > 99) {
+                attacker.combat.claimsVictory(CombatEndType.Lust, defender);
+                this.defeatEvents.push(new DefeatEvent(attacker, defender, CombatEndType.Lust));
+            }
+            else if (attacker.combat.hasDefeated(defender)) {
+                attacker.combat.claimsVictory(CombatEndType.Special, defender);
+                this.defeatEvents.push(new DefeatEvent(attacker, defender, CombatEndType.Special));
+            }
+        }
     }
 }
 
@@ -56,36 +74,41 @@ export default class CombatManager {
     private player: Player;
     private playerParty: Character[];
     private monsterParty: Character[];
+    private combatCleanUp: boolean;
     private specialEnding: SpecialEnding;
     public playerCombatParty: CombatParty;
     public monsterCombatParty: CombatParty;
 
-    public constructor(player: Player, playerParty: Character[], monsterParty: Character[], specialEnding: SpecialEnding = null) {
+    public constructor(player: Player, playerParty: Character[], monsterParty: Character[], specialEnding: SpecialEnding = null, combatCleanUp: boolean = true) {
         this.player = player;
         this.playerParty = playerParty;
         this.monsterParty = monsterParty;
         this.playerCombatParty = new CombatParty(playerParty.concat(player));
         this.monsterCombatParty = new CombatParty(monsterParty);
+
+        this.combatCleanUp = combatCleanUp;
+        this.specialEnding = specialEnding;
     }
 
     public performCombatRound() {
+        const player = this.playerCombatParty.readyPartyMember();
+        const enemy = this.monsterCombatParty.readyPartyMember();
         if (this.playerCombatParty.ableMembers.length > 0 && this.monsterCombatParty.ableMembers.length > 0) {
-            if (this.playerCombatParty.ableMembers[0].charType == CharacterType.Player) {
+            if (player.charType == CharacterType.Player) {
                 // player pick action
                 CombatMenu.display(this.player);
             }
             else {
                 //do ai
-                this.playerCombatParty.ableMembers[0];
+                player;
             }
-            this.resolveCombatAction(this.playerCombatParty, this.monsterCombatParty);
-            this.playerCombatParty.updateMembers();
+            this.monsterCombatParty.resolveAttacker(player);
         }
 
         if (this.playerCombatParty.ableMembers.length > 0 && this.monsterCombatParty.ableMembers.length > 0) {
-            this.monsterCombatParty.ableMembers[0];
-            this.resolveCombatAction(this.monsterCombatParty, this.playerCombatParty);
-            this.monsterCombatParty.updateMembers();
+            //do ai
+            enemy;
+            this.playerCombatParty.resolveAttacker(enemy);
         }
         this.resolveCombatRound();
     }
@@ -103,32 +126,21 @@ export default class CombatManager {
         
     }*/
 
-    private resolveCombatAction(attackingParty: CombatParty ,defendingParty: CombatParty) {
-        const attacker: Character = attackingParty.ableMembers[0];
-        const defender: Character = defendingParty.ableMembers[0];
-        if (defender.stats.HP < 1) {
-            attacker.combat.claimsVictory(CombatEndType.HP, defender);
-            defendingParty.lostFight(CombatEndType.HP, attacker);
-        }
-        else if (defender.stats.lust > 99) {
-            attacker.combat.claimsVictory(CombatEndType.Lust, defender);
-            defendingParty.lostFight(CombatEndType.Lust, attacker);
-        }
-        else if (attacker.combat.hasDefeated(defender)) {
-            attacker.combat.claimsVictory(CombatEndType.Special, defender);
-            defendingParty.lostFight(CombatEndType.Special, attacker);           
-        }
-    }
-
     private resolveCombatRound() {
+        this.playerCombatParty.readyNextPartyMember();
+        this.monsterCombatParty.readyNextPartyMember();
         CombatUpdate.combatStatusAffectsUpdate(this.playerCombatParty.ableMembers, this.monsterCombatParty.ableMembers);
         if (this.playerCombatParty.ableMembers.length == 0 || this.monsterCombatParty.ableMembers.length == 0) {
             if (!this.specialEnding) {
                 this.displayDefeatEvents();
-                CombatEnd.combatCleanup(this.player, this.playerParty, this.monsterParty);
+                if (this.combatCleanUp)
+                    CombatEnd.combatCleanup(this.player, this.playerParty, this.monsterParty);
             }
-            else
+            else {
+                if (this.combatCleanUp)
+                    CombatEnd.combatCleanup(this.player, this.playerParty, this.monsterParty);
                 this.specialEnding(this.player, this.playerParty, this.monsterParty);
+            }
         }
     }
 
