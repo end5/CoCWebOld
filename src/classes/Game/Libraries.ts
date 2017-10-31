427bcf360e9f09b9c244525d6a89b001b82eb484
end5 <16655171+end5@users.noreply.github.com>
2017-10-30 21:00:45 -0700

    Libraries updated to use new itemtype

--- src/classes/Game/Libraries.ts
+++ src/classes/Game/Libraries.ts
@@ -1,25 +1,45 @@
-﻿import Library from "../Utilities/Library";
-import ConsumableLib from "../Items/Consumables/ConsumableLib";
-import WeaponLib from "../Items/Weapons/WeaponLib";
-import ArmorLib from "../Items/Armors/ArmorLib";
-import StatusAffectDescLib from "../Effects/StatusAffectDescLib";
-import PerkDescLib from "../Effects/PerkDescLib";
-import MaterialLib from "../Items/MaterialLib";
+﻿import PerkDescLib from '../Effects/PerkDescLib';
+import StatusAffectDescLib from '../Effects/StatusAffectDescLib';
+import ArmorLib from '../Items/Armors/ArmorLib';
+import ConsumableLib from '../Items/Consumables/ConsumableLib';
+import { ItemType } from '../Items/Item';
+import MaterialLib from '../Items/MaterialLib';
+import WeaponLib from '../Items/Weapons/WeaponLib';
+import Library from '../Utilities/Library';
 
 export default class Libraries {
-    public readonly weapons: WeaponLib;
-    public readonly armor: ArmorLib;
-    public readonly consumables: ConsumableLib;
-    public readonly materials: MaterialLib;
     public readonly statusAffectDesc: StatusAffectDescLib;
     public readonly perkDesc: PerkDescLib;
+    private readonly items: object;
 
     public constructor() {
-        this.weapons = new WeaponLib();
-        this.armor = new ArmorLib();
-        this.consumables = new ConsumableLib();
-        this.materials = new MaterialLib();
+        this.items = {};
+        this.items[ItemType.Armor] = new ArmorLib();
+        this.items[ItemType.Consumable] = new ConsumableLib();
+        this.items[ItemType.KeyItem] = new ArmorLib();
+        this.items[ItemType.Material] = new MaterialLib();
+        this.items[ItemType.Weapon] = new WeaponLib();
         this.statusAffectDesc = new StatusAffectDescLib();
         this.perkDesc = new PerkDescLib();
+    }
+
+    public get armor(): ArmorLib {
+        return this.items[ItemType.Weapon];
+    }
+    public get consumables(): ConsumableLib {
+        return this.items[ItemType.Consumable];
+    }
+    public get keyItem(): WeaponLib {
+        return this.items[ItemType.KeyItem];
+    }
+    public get materials(): MaterialLib {
+        return this.items[ItemType.Material];
+    }
+    public get weapon(): WeaponLib {
+        return this.items[ItemType.Weapon];
+    }
+
+    public lookupItem(itemKey: string, itemType: ItemType): any {
+        return this.items[itemType].get(itemKey);
     }
 }
