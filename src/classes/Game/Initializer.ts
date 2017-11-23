ed3ce3cea3f13b0238c3fc243dbe641ebb6ccd0f
end5 <16655171+end5@users.noreply.github.com>
2017-11-22 22:51:11 -0800

    Added Initializer

---
+++ src/classes/Game/Initializer.ts
@@ -0,0 +1,11 @@
+﻿import PerkFactory from '../Effects/PerkFactory';
+import StatusAffectFactory from '../Effects/StatusAffectFactory';
+import ItemFactory from '../Items/ItemFactory';
+
+export default class Initializer {
+    public constructor() {
+        new ItemFactory();
+        new PerkFactory();
+        new StatusAffectFactory();
+    }
+}
