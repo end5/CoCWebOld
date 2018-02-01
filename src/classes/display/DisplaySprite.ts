6d51f8f8f19eb670e9d583e83ef8ee5ba9d85080
end5 <16655171+end5@users.noreply.github.com>
2018-01-31 19:17:14 -0800

    Updated DisplaySprite to work like DisplayText.

--- src/classes/display/DisplaySprite.ts
+++ src/classes/display/DisplaySprite.ts
@@ -1,6 +1,13 @@
 import ImageElement from './Elements/ImageElement';
+import SpriteLibrary from './Images/SpriteLibrary';
+import SpriteName from './Images/SpriteName';
 import { Utils } from '../Utilities/Utils';
 
-const DisplaySprite: ImageElement = new ImageElement();
-DisplaySprite.setHTMLElement(Utils.loadFromId("mainSpriteDisplay") as HTMLImageElement);
-export default DisplaySprite;
+const spriteElement: ImageElement = new ImageElement();
+spriteElement.setHTMLElement(Utils.loadFromId("mainSpriteDisplay") as HTMLImageElement);
+const spriteLib: SpriteLibrary = new SpriteLibrary();
+
+export default function DisplaySprite(name: SpriteName) {
+    spriteElement.load(spriteLib.get(name));
+    spriteElement.show();
+}
