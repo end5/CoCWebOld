78a879ab85047c96540061d5084d9a4b085c1c9f
end5 <16655171+end5@users.noreply.github.com>
2018-02-04 19:20:32 -0800

    KeyItems added to CharacterInventory.

--- src/classes/Inventory/CharacterInventory.ts
+++ src/classes/Inventory/CharacterInventory.ts
@@ -4,25 +4,30 @@ import Inventory from './Inventory';
 import Character from '../Character/Character';
 import Armor from '../Items/Armors/Armor';
 import Item from '../Items/Item';
+import KeyItem from '../Items/KeyItem';
 import Weapon from '../Items/Weapons/Weapon';
 import ISerializable from '../Utilities/ISerializable';
+import SerializableDictionary from '../Utilities/SerializableDictionary';
 
 export default class CharacterInventory implements ISerializable<CharacterInventory> {
     public readonly items: Inventory<Item>;
     public readonly equipment: EquipmentInventory;
     public gems: number;
+    public readonly keyItems: SerializableDictionary<KeyItem>;
 
     public constructor(character: Character) {
         this.items = new Inventory<Item>();
         this.equipment = new EquipmentInventory(character);
         this.gems = 0;
+        this.keyItems = new SerializableDictionary(KeyItem);
     }
 
     public serialize(): string {
         return JSON.stringify({
             gems: this.gems,
             items: this.items.serialize(),
-            equipment: this.equipment.serialize()
+            equipment: this.equipment.serialize(),
+            keyItems: this.keyItems.serialize()
         });
     }
 
@@ -30,5 +35,6 @@ export default class CharacterInventory implements ISerializable<CharacterInvent
         this.gems = saveObject.gems;
         this.items.deserialize(saveObject.items);
         this.equipment.deserialize(saveObject.equipment);
+        this.keyItems.deserialize(saveObject.keyItems);
     }
 }
