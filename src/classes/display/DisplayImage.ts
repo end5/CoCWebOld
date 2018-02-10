6c571414b2a8a32a0ca347a624f1e11e9c8b870a
end5 <16655171+end5@users.noreply.github.com>
2018-02-09 18:18:23 -0800

    DisplayImage now works

--- src/classes/display/DisplayImage.ts
+++ src/classes/display/DisplayImage.ts
@@ -1,6 +1,13 @@
 import ImageElement from './Elements/ImageElement';
+import ImageLibrary from './Images/ImageLibrary';
+import ImageName from './Images/ImageName';
 import { Utils } from '../Utilities/Utils';
 
-const DisplayImage: ImageElement = new ImageElement();
-DisplayImage.setHTMLElement(Utils.loadFromId("mainImageDisplay") as HTMLImageElement);
-export default DisplayImage;
+const imageElement: ImageElement = new ImageElement();
+imageElement.setHTMLElement(Utils.loadFromId("mainImageDisplay") as HTMLImageElement);
+const imageLib: ImageLibrary = new ImageLibrary();
+
+export default function DisplayImage(name: ImageName) {
+    imageElement.load(imageLib.get(name));
+    imageElement.show();
+}
