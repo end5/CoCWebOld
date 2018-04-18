a68277de4164ee705f5acaa6a61d2ef79aef64c9
end5 <16655171+end5@users.noreply.github.com>
2018-04-17 23:26:05 -0700

    Fixed image loading.

--- build/Engine/Display/DisplayImage.ts
+++ build/Engine/Display/DisplayImage.ts
@@ -1,17 +1,17 @@
 import ImageElement from './Elements/ImageElement';
-import ImageLibrary from './Images/ImageLibrary';
+import { getImage, loadImages } from './Images/ImageLibrary';
 import ImageName from './Images/ImageName';
 import { loadFromId } from '../Utilities/Html';
 
 const imageElement: ImageElement = new ImageElement();
 imageElement.setHTMLElement(loadFromId("mainImageDisplay") as HTMLImageElement);
-const imageLib: ImageLibrary = new ImageLibrary();
+loadImages();
 
 export default function DisplayImage(name: ImageName) {
     if (name === ImageName.None)
         imageElement.hide();
     else {
-        imageElement.load(imageLib.get(name));
+        imageElement.load(getImage(name));
         imageElement.show();
     }
 }
