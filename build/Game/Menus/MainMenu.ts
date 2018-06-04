e936ca9bb65d2cb240e393ba8e01d97714670aa9
end5 <16655171+end5@users.noreply.github.com>
2018-06-04 16:54:03 -0700

    Updated main menu with version info and proper title.

--- build/Game/Menus/MainMenu.ts
+++ build/Game/Menus/MainMenu.ts
@@ -5,6 +5,7 @@ import { clickFuncWrapper, NextScreenChoices } from '../ScreenDisplay';
 import { Settings } from '../Settings';
 import { User } from '../User';
 import { isEaster, isValentine } from '../Utilities/Dates';
+import { VersionInfo } from '../VersionInfo';
 
 export function display(): NextScreenChoices {
     if (!User.char)
@@ -19,35 +20,37 @@ export function display(): NextScreenChoices {
     MainScreen.getTopButton(TopButton.Data).modify("Data", clickFuncWrapper(Menus.Data));
 
     DisplayText().clear();
-    DisplayText("Corruption of Champions (0.9.4c)").bold().newline();
-    DisplayText("Release Build").newline();
+    DisplayText("Corruption of Champions Web Edition v" + VersionInfo.number).bold().endline();
+    DisplayText(VersionInfo.stability + " Build").endline();
 
-    DisplayText(`<![CDATA[
-        <br>(Formerly Unnamed Text Game)
-        <u>Created by: Fenoxo</u>
-
-        Edited By:<br>
-        &nbsp; &nbsp; &nbsp; Ashi, SoS, Prisoner416, Zeikfried, et al
-
-        Open-source contributions by:<br>
-        &nbsp; &nbsp; &nbsp; aimozg, Amygdala, Cmacleod42, Enterprise2001, Fake-Name, Gedan, Yoffy, et al
-
-        Source Code: <u><a href='https://github.com/herp-a-derp/Corruption-of-Champions'>https://github.com/herp-a-derp/Corruption-of-Champions</a></u>
-
-        Bug Tracker: <u><a href='https://github.com/herp-a-derp/Corruption-of-Champions/issues'>https://github.com/herp-a-derp/Corruption-of-Champions/issues</a></u>
+    DisplayText(`
+        This is a port of the original Corruption of Champions (0.9.4c) by end5.
+        <br>
+        <br>Original Version Credits:
+        <br><u>Created by: Fenoxo</u>
+        <br>
+        <br>Edited By:
+        <br>Ashi, SoS, Prisoner416, Zeikfried, et al
+        <br>
+        <br>Open-source contributions by:
+        <br>aimozg, Amygdala, Cmacleod42, Enterprise2001, Fake-Name, Gedan, Yoffy, et al
+        <br>
+        <br>Source Code: <u><a href='https://github.com/herp-a-derp/Corruption-of-Champions'>https://github.com/herp-a-derp/Corruption-of-Champions</a></u>
+        <br>
+        <br>Bug Tracker: <u><a href='https://github.com/herp-a-derp/Corruption-of-Champions/issues'>https://github.com/herp-a-derp/Corruption-of-Champions/issues</a></u>
         (requires an account, unfortunately)
-
-        **<u>DISCLAIMER</u>**
+        <br>
+        <br>**<u>DISCLAIMER</u>**
         <br>- **There are many strange and odd fetishes contained in this flash.  Peruse at own risk.**
         <br>- **Please be 18 or the legal age to view porn before playing.**
         <br>- **Try to keep your keyboard clean.  Think of the children!**
+        <br>
+        <br>
+        <br>For more information see Fenoxo's Blog at <b><u><a href='http://www.fenoxo.com/'>fenoxo.com</a></u></b>.
+        <br>
+        <br>Also go play <u><a href='http://www.furaffinity.net/view/9830293/'>Nimin</a></u> by Xadera on furaffinity.
 
-
-        For more information see Fenoxo's Blog at <b><u><a href='http://www.fenoxo.com/'>fenoxo.com</a></u></b>.
-
-        Also go play <u><a href='http://www.furaffinity.net/view/9830293/'>Nimin</a></u> by Xadera on furaffinity.
-
-	    ]]>`);
+	    `);
 
     if (User.settings.debug)
         DisplayText("\n\n<b>DEBUG MODE ENABLED:  ITEMS WILL NOT BE CONSUMED BY USE.</b>");
